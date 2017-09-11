249a9ac0dab414c79c9295d44f09411f472a0078
iliasso <ilias8@live.nl>
2017-09-11 12:30:34 +0200

    hw inleveren

---
+++ les_1/les_1 opdr.py
@@ -0,0 +1,25 @@
+vraag 1.2
+a 34 letters
+b ja (true)
+c nee (false)
+d componist =  ['Berlioz', 'Borodin', 'Brian', 'Bartok', 'Bellini', 'Buxtehude', 'Bernstein'] sorted(componist)
+
+vraag 1.3
+1. a = 6 en b =7
+2. c = 0.5*a + 0.5*b
+3. inventaris = 'papier, nietjes en pennen'
+4. voornaam = 'Ilias'  achternaam = 'Elbarjaj'
+5. mijnnaam = voornaam + ' ' + achternaam
+
+vraag 1.4
+1. 6.75 > a and 6.75 < b
+2. inventaris > 5*mijnnaam
+3. inventaris = 0 or inventaris > 10
+
+vraag 1.5
+1. favorieten = ['lil kleine']
+2. favorieten.append('boef')
+3. favorieten[1] = 'frenna'
+
+vraag 1.6
+?
